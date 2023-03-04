4adf5c57b8559e7f9cfdbe992da1f1852980940a
Ivan Dimitrov <gmivanko@abv.bg>
2023-03-04 23:53:12 +0200

    Add files via upload

---
+++ Programming Basics with JavaScript/First Steps In Coding/3.05.number100...200.js
@@ -0,0 +1,11 @@
+function number100(input){
+    let number = Number(input[0]);
+    if (number < 100) {
+        console.log("Less than 100");
+    } else if (number <=200) {
+        console.log("Between 100 and 200");
+    } else if (number > 200) 
+        console.log("Greater than 200") 
+}
+
+number100(["342"])
